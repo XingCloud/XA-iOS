341b5af008361c60450ceda227e592d003426a04
zhaoshihun <zsh019@gmail.com>
2012-02-17 19:22:50 +0800

    add demo

---
+++ XAdemo/XAdemo/AppDelegate.mm
@@ -0,0 +1,79 @@
+//
+//  AppDelegate.m
+//  XAdemo
+//
+//  Created by mini xingcloud on 12-2-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [_window release];
+    [super dealloc];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    [XA setDelegate:self];
+    [XA setLogEnabled:YES];
+    [XA applicationDidLaunched];
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    [XA applicationWillResignActive];
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    /*
+     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+     */
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    [XA applicationDidBecomeActive];
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    [XA applicationWillTerminate];
+    /*
+     Called when the application is about to terminate.
+     Save data if appropriate.
+     See also applicationDidEnterBackground:.
+     */
+}
+- (NSString *)appId
+{
+    return @"";
+}
+- (NSString *)channelId
+{
+    return @"";
+}
+@end
