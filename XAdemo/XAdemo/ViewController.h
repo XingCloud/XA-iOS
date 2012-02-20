f5b7a6bf5d2604ec21587e8555ed012de9a0221d
zhaoshihun <zsh019@gmail.com>
2012-02-20 10:35:32 +0800

    add demo

---
+++ XAdemo/XAdemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XAdemo
+//
+//  Created by mini xingcloud on 12-2-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
