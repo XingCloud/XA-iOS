753d1f76a57909a05b7b83fe334bb9a7ce7d95e7
zhao shihun <zsh019@gmail.com>
2012-03-26 12:03:50 +0800

    stop curl when backgroud

---
+++ XA/curl/ciciCurl.cpp
@@ -0,0 +1,9 @@
+//
+//  ciciCurl.cpp
+//  XA
+//
+//  Created by mini xingcloud on 12-3-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ XA/curl/ciciCurl.h
@@ -0,0 +1,14 @@
+//
+//  ciciCurl.h
+//  XA
+//
+//  Created by mini xingcloud on 12-3-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef XA_ciciCurl_h
+#define XA_ciciCurl_h
+
+
+
+#endif
